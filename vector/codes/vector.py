ea2c4a7bce36fc6f52f928a732aa3f92c25d03c6
SURAJIT SARKAR <113087664+sssurajit@users.noreply.github.com>
2022-12-31 20:12:20 +0530

    Add files via upload

---
+++ vector/codes/vector.py
@@ -0,0 +1,43 @@
+#Drawing a rectangle
+
+import numpy as np
+import matplotlib.pyplot as plt
+from coeffs import *
+
+#if using termux
+import subprocess
+import shlex
+#end if
+
+#Rectangle vertices
+A = np.array([0,0]) 
+B = np.array([36,15])
+c = np.array([15,0])
+
+#Generating all lines
+x_AB = line_gen(A,B)
+x_BC = line_gen(B,C)
+x_CA = line_gen(C,A)
+
+#Plotting all lines
+plt.plot(x_AB[0,:],x_AB[1,:],label='$AB$')
+plt.plot(x_BC[0,:],x_BC[1,:],label='$BC$')
+plt.plot(x_CD[0,:],x_CD[1,:],label='$CA$')
+
+plt.plot(A[0], A[1], 'o')
+plt.text(A[0] * (1 + 0.1), A[1] * (1 - 0.1) , 'A')
+plt.plot(B[0], B[1], 'o')
+plt.text(B[0] * (1 - 0.2), B[1] * (1) , 'B')
+plt.plot(C[0], C[1], 'o')
+plt.text(C[0] * (1 + 0.03), C[1] * (1 - 0.1) , 'C')
+
+plt.xlabel('$x$')
+plt.ylabel('$y$')
+plt.legend(loc='best')
+plt.grid() # minor
+
+#if using termux                        
+plt.savefig('/sdcard/IITH/vector/vector.pdf')                                                                
+subprocess.run(shlex.split("termux-open /sdcard/IITH/vector/vector.pdf")) 
+#else       
+#plt.show()
