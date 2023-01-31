e1d19dee1cfa2f88333122a971f43cc5a5964b48
SURAJIT SARKAR <113087664+sssurajit@users.noreply.github.com>
2023-01-31 16:18:26 +0530

    Update code.py

--- lines/11.10.2.8/codes/code.py
+++ lines/11.10.2.8/codes/code.py
@@ -4,37 +4,33 @@ from numpy import linalg as LA
 import sys
 lib_path = '/sdcard/IITH/matrix/CoordGeo'
 sys.path.insert(0,lib_path)
-
 #local import
 from line.funcs import *
-
-
 #if using termux
 import subprocess
 import shlex
 #end if
 
-
-#Point A and B
-A = np.array([10/np.sqrt(3),0]) 
-B = np.array([0,10]) 
-O = np.array([0,0])
-M = np.array([5*np.sqrt(3)/2,5/2])
-#Generating all lines
-x_AB = line_gen(A,B)
-x_OM =line_gen(O,M)
-
-#Plotting all lines
-plt.plot(x_AB[0,:],x_AB[1,:],label='$Line$')
-plt.plot(A[0], A[1])
-#plt.text(A[0] * (1 + 0.1), A[1] * (1 - 0.1) , 'A')
-plt.plot(B[0], B[1])
-#plt.text(B[0] * (1 - 0.1), B[1] * (1) , 'B')
-plt.plot(x_OM[0,:],x_OM[1,:],label='$Perpendicular-from-Origin$')
-plt.plot(O[0], O[1])
-plt.text(O[0] * (1 + 0.5), O[1] * (1 - 0.5) , 'O')
-plt.plot(M[0], M[1], 'o')
-#plt.text(M[0] * (1 - 0.2), M[1] * (1) , 'M')
+m = np.array([1,np.tan(np.deg2rad(30))])
+i = np.array([np.cos(np.deg2rad(30))])
+s = np.array(([0,1],[-1,0]))                             
+n = s@m   
+d = 5 
+P = d / i
+print(P)
+#Input parameters  
+A =  np.array(([10/np.sqrt(3),0]))
+I =  np.array(([5*np.cos(np.deg2rad(30)),5*np.sin(np.deg2rad(30))]))
+O =  np.array(([0,0])) 
+n=m@s
+#Generating the line   
+k1=8
+k2=0
+x_AB = line_dir_pt(n,A,k1,k2)
+x_OI = line_gen(O,I)
+#Plotting all lines  
+plt.plot(x_AB[0,:],x_AB[1,:],label='line')   
+plt.plot(x_OI[0,:],x_OI[1,:],label='d=5')    
 
 
 plt.xlabel('$ X $')
